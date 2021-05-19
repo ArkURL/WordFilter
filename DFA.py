4511607a6ec2c9fabf634b4dae23daedb1e3060c
ArkURL <1035096867@qq.com>
2021-05-19 23:15:50 +0800

    'all'

---
+++ DFA.py
@@ -0,0 +1,64 @@
+# -*- coding:utf-8 -*-
+# @Time:2021/5/19 21:00
+# @Author:ui_none
+
+import copy
+
+
+class DFA:
+    def __init__(self, keyword_list: list):
+        self.state_event_dict = self._generate_state_event_dict(keyword_list)
+
+    def match(self, state_event_dict: dict, content: str):
+        match_list = []
+        state_list = []
+        temp_match_list = []
+
+        for char_pos, char in enumerate(content):
+            if char in state_event_dict:
+                state_list.append(state_event_dict)
+                temp_match_list.append({
+                    "start": char_pos,
+                    "match": "",
+                    "length": 0
+                })
+
+            for index, state in enumerate(state_list):
+                if char in state:
+                    state_list[index] = state[char]
+                    temp_match_list[index]["match"] += char
+                    temp_match_list[index]["length"] += 1
+
+                    if state[char]["is_end"]:
+                        match_list.append(copy.deepcopy(temp_match_list[index]))
+
+                        if len(state[char].keys()) == 1:
+                            state_list.pop(index)
+                            temp_match_list.pop(index)
+                else:
+                    state_list.pop(index)
+                    temp_match_list.pop(index)
+
+        return match_list
+
+    @staticmethod
+    def _generate_state_event_dict(keyword_list: list) -> dict:
+        state_event_dict = {}
+
+        for keyword in keyword_list:
+            current_dict = state_event_dict
+            length = len(keyword)
+
+            for index, char in enumerate(keyword):
+                if char not in current_dict:
+                    next_dict = {"is_end": False}
+                    current_dict[char] = next_dict
+                    current_dict = next_dict
+                else:
+                    next_dict = current_dict[char]
+                    current_dict = next_dict
+
+                if index == length - 1:
+                    current_dict["is_end"] = True
+
+        return state_event_dict
---
+++ common.py
@@ -0,0 +1,48 @@
+# -*- coding:utf-8 -*-
+# @Time:2021/5/19 19:19
+# @Author:ui_none
+
+import pickle as pik
+import json
+
+
+def fun():
+    print('test')
+
+
+# 从txt文件中读取多行数据并转换为列表
+def read_multiple_lines_from_txt(path_to_file:str='.\\text.txt') -> list:
+    with open(path_to_file, 'r', encoding='utf-8') as f:
+        total_list = []
+        for each_str in f.readlines():
+            each_str = each_str.strip('\n')  # 返回str
+            each_list = each_str.split(',')  # 返回处理后的列表
+            total_list.extend(each_list)     # 用each_list拓展total_list
+        return total_list
+
+
+# 从txt文件读取单行数据并转换为列表
+def read_single_line_from_txt(path_to_file:str='.\\text.txt') -> list:
+    with open(path_to_file, 'r', encoding='utf-8') as f:
+        s = f.read()
+        keyword_list = s.split(',')
+        return keyword_list
+
+
+# 保存制作好的HashMap
+def save_hash_map(hash_map_list: list, output_file_path: str = '.\\hashmap.json') -> None:
+    with open(output_file_path, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(hash_map_list))
+
+
+# 保存dfa对象
+def save_dfa_object(dfa_object, output_file_path: str = '.\\dfa_object.pik') -> None:
+    with open(output_file_path, 'wb+') as f:
+        pik.dump(dfa_object, f)
+
+
+# 从外部文件加载content文本
+def load_content(path_to_content: str = '.\\content.txt') -> str:
+    with open(path_to_content, 'r', encoding='utf-8') as f:
+        content = f.read()
+        return content
