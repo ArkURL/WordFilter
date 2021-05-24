30b4d0fa90ad0f4300b44722448f42a6af7709cf
ArkURL <1035096867@qq.com>
2021-05-24 10:42:34 +0800

    make some changes

--- main.py
+++ main.py
@@ -5,17 +5,17 @@
 
 import re
 import time
-import common
+import com
 import data_process
 
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
     # content = "信息抽取之 DFA 算法匹配关键词，匹配算法，信息抓取"
-    content = common.load_content()
+    content = com.load_content()
     content_list = list(content)
     # keyword_list = ["信息抽取", "匹配", "匹配关键词", "匹配算法"]
-    keyword_list = common.read_multiple_lines_from_txt('keywords.txt')
+    keyword_list = com.read_multiple_lines_from_txt('.\\input\\keywords.txt')
 
     state_dict = data_process.generate_state_event_dict(keyword_list)  # 获取关键字列表并生成hash_map
     res_match_list = data_process.match(state_dict, content)  # 根据hash_map和content生成匹配列表
